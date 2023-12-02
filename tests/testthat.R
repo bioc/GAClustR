5b86907ba0a269a1b868082b92e5ccb8dc8dadba
Frank Chen <35710760+frankcsquared@users.noreply.github.com>
2023-12-02 14:52:29 -0500

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(GAclustenhancer)
+library(GAClustR)
 
-test_check("GAclustenhancer")
+test_check("GAClustR")
